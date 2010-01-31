d4bb086cecad9f12defcbe712cc28cc569fc4fe4
Haiko Schol <hs@zeropatience.net>
2010-01-31 22:11:25 +0100

    doesn't work either

--- scripts/deploy_from_ci.sh
+++ scripts/deploy_from_ci.sh
@@ -1,36 +1,19 @@
 #!/bin/bash
 
-#export PIP_DOWNLOAD_CACHE=$HOME/pip_download_cache
+export PIP_DOWNLOAD_CACHE=$HOME/pip_download_cache
 
-#if [ -n "${WORKSPACE:-x}" ]; then
-#    WORKSPACE="$PWD"
-#fi
+if [ -n "${WORKSPACE:-x}" ]; then
+    WORKSPACE="$PWD"
+fi
 
-#cd $WORKSPACE
+cd $WORKSPACE
 
-#python bootstrap.py --deploy minddrag-django-env
+python bootstrap.py --deploy minddrag-django-env
 
-#. minddrag-django-env/bin/activate
-
-#fab deploy > deploy_log.txt
-#DEPLOY_STATUS = $?
-#fab update_docs >> deploy_log.txt
-#exit $DEPLOY_STATUS
-
-# ***FIXME*** kludgey workaround! this assumes that hudson and
-# the minddrag django app run on the same box! hardcoded paths!
-# ugly hackery galore!
-
-cd /tmp
-sudo su minddrag
-cd /home/minddrag/minddrag-django
 . minddrag-django-env/bin/activate
-hg pull
-hg update
-pip install -r requirements.txt
-pip install -r prod-requirements.txt
-touch minddrag/apache/django.wsgi
 
-cd docs
-make html
+fab deploy > deploy_log.txt
+DEPLOY_STATUS = $?
+fab update_docs >> deploy_log.txt
+exit $DEPLOY_STATUS
 
