db31308eeef53f036afb4fbdaf6212600cc06d69
Haiko Schol <hs@zeropatience.net>
2010-01-31 22:40:34 +0100

    forgot database name...

--- settings/romulus_settings.py
+++ settings/romulus_settings.py
@@ -3,6 +3,7 @@ TEMPLATE_DEBUG = False
 
 DATABASE_ENGINE = 'mysql'
 DATABASE_OPTIONS = {'init_command': 'SET storage_engine=INNODB'}
+DATABASE_NAME = 'minddrag'
 DATABASE_USER = 'minddrag'
 DATABASE_PASSWORD = 'EnikniOjkogMoiHivRew'
 DATABASE_HOST = 'localhost'
