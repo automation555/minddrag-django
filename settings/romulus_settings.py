685a8d6668c7f73b207591946de76b91fa872ae6
Haiko Schol <hs@zeropatience.net>
2010-02-14 01:09:31 +0100

    switched some more to postgres...

--- settings/romulus_settings.py
+++ settings/romulus_settings.py
@@ -1,8 +1,7 @@
 DEBUG = False
 TEMPLATE_DEBUG = False
 
-DATABASE_ENGINE = 'mysql'
-DATABASE_OPTIONS = {'init_command': 'SET storage_engine=INNODB'}
+DATABASE_ENGINE = 'postgresql_psycopg2'
 DATABASE_NAME = 'minddrag'
 DATABASE_USER = 'minddrag'
 DATABASE_PASSWORD = 'EnikniOjkogMoiHivRew'
